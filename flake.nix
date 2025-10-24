3e19859e180f2abdfaeb79b84a72821ec2d751d2
Florian Beeres <yuuki@protonmail.com>
2025-10-24 12:20:50 +0200

    Update flake + flake parts

--- flake.nix
+++ flake.nix
@@ -2,35 +2,61 @@
   description = "Advent of Code in Janet";
 
   inputs = {
-    # Newer nixpkgs break my Git :(
-    # TODO: Find broken commit
-    nixpkgs.url = "github:NixOS/nixpkgs?rev=36fcc808a13782146ab0549ad52c27551af5c49f";
-    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    flake-parts.url = "github:hercules-ci/flake-parts";
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    flake-utils,
-  }:
-    flake-utils.lib.eachDefaultSystem (
-      system: let
-        pkgs = import nixpkgs {
-          inherit system;
+  outputs =
+    inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [
+        # To import a flake module
+        # 1. Add foo to inputs
+        # 2. Add foo as a parameter to the outputs function
+        # 3. Add here: foo.flakeModule
+
+      ];
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "aarch64-darwin"
+        "x86_64-darwin"
+      ];
+      perSystem =
+        {
+          config,
+          self',
+          inputs',
+          pkgs,
+          system,
+          ...
+        }:
+        {
+          # Per-system attributes can be defined here. The self' and inputs'
+          # module parameters provide easy access to attributes of the same
+          # system.
+
+          # Equivalent to  inputs'.nixpkgs.legacyPackages.hello;
+          # packages.default = pkgs.hello;
+          devShells.default = pkgs.mkShell {
+            packages = [
+            pkgs.coreutils
+			pkgs.nixfmt
+            pkgs.jq
+            pkgs.alejandra
+            pkgs.tokei
+            pkgs.janet
+            pkgs.jpm
+            pkgs.nodePackages.prettier
+            pkgs.graphviz
+            ];
+          };
         };
-      in {
-        devShell = pkgs.mkShell {
-          buildInputs = with pkgs; [
-            coreutils
-            jq
-            alejandra
-            tokei
-            janet
-            jpm
-            nodePackages.prettier
-            graphviz
-          ];
-        };
-      }
-    );
+      flake = {
+        # The usual flake attributes can be defined here, including system-
+        # agnostic ones like nixosModule and system-enumerating ones, although
+        # those are more easily expressed in perSystem.
+
+      };
+    };
 }
